7396418a4b4ad8fefe714eb66b76ae77be6af819
Alex Berka <alexanderberka@gmail.com>
2025-09-05 09:38:16 -0500

    Detroit historical weather

---
+++ sqlmesh/models/raw/raw_detroit_weather.sql
@@ -0,0 +1,16 @@
+MODEL(
+  name raw.detroit_weather,
+  kind FULL,
+  gateway duckdb
+);
+
+SET s3_region='us-east-1';
+SET s3_access_key_id='miniouser';
+SET s3_secret_access_key='miniopassword';
+SET s3_endpoint='host.docker.internal:9000';
+SET s3_url_style='path';
+SET s3_use_ssl=false;
+
+SELECT
+  *
+FROM read_csv('s3://wcb/weather/detroit/detroit_weather_report_2025-09-03.csv', skip=1, header=True);
